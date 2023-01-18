aeee3d085b6d8788a308d7d012a58e970be91998
Jeronymous <jeromelouradour@gmail.com>
2023-01-18 23:10:01 +0100

    simplify setup

--- setup.py
+++ setup.py
@@ -1,13 +1,8 @@
 import os
 
-import pkg_resources
 from setuptools import setup, find_packages
 
 install_requires = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
-dependency_links = [
-    # Note: to avoid confusion between https://pypi.org/project/whisper/ and OpenAI's whisper
-    "git+https://github.com/openai/whisper.git@main#egg=openai-whisper"
-]
 
 setup(
     name="whisper-timestamped",
@@ -21,7 +16,6 @@ setup(
     license="MIT",
     packages=find_packages(exclude=["tests*"]),
     install_requires=install_requires,
-    dependency_links=dependency_links,
     entry_points = {
         'console_scripts': ['whisper_timestamped=whisper_timestamped.transcribe:cli'],
     },
