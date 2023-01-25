8751846dbada4720bd014881df9dc82f3cbba17f
Jeronymous <jeromelouradour@gmail.com>
2023-01-25 22:17:00 +0100

    fix license in setup

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ from setuptools import setup, find_packages
 install_requires = open(os.path.join(os.path.dirname(__file__), "requirements.txt")).readlines()
 
 version = None
+license = None
 with open(os.path.join(os.path.dirname(__file__), "whisper_timestamped", "transcribe.py")) as f:
     for line in f:
         if line.strip().startswith("__version__"):
             version = line.split("=")[1].strip().strip("\"'")
-            break
-assert version
+            if version and license:
+                break
+        if line.strip().startswith("__license__"):
+            license = line.split("=")[1].strip().strip("\"'")
+            if version and license:
+                break
+assert version and license
 
 setup(
     name="whisper-timestamped",
@@ -18,10 +24,10 @@ setup(
     version=version,
     description="Add to OpenAI Whisper the capability to give word timestamps",
     readme="README.md",
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     author="Jeronymous",
     url="https://github.com/Jeronymous/whisper-timestamped",
-    license="MIT",
+    license=license,
     packages=find_packages(exclude=["tests*"]),
     install_requires=install_requires,
     entry_points = {
