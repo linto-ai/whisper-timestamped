fb3302469996a9339fbfcd00fe90fbaaed034edd
Jeronymous <jeromelouradour@gmail.com>
2023-02-23 10:26:19 +0100

    do not import whisper just for simple utils

--- whisper_timestamped/make_subtitles.py
+++ whisper_timestamped/make_subtitles.py
@@ -64,6 +64,44 @@ def split_long_segments(segments, max_length, use_space = True):
             
     return new_segments
 
+def format_timestamp(seconds: float, always_include_hours: bool = False, decimal_marker: str = '.'):
+    assert seconds >= 0, "non-negative timestamp expected"
+    milliseconds = round(seconds * 1000.0)
+
+    hours = milliseconds // 3_600_000
+    milliseconds -= hours * 3_600_000
+
+    minutes = milliseconds // 60_000
+    milliseconds -= minutes * 60_000
+
+    seconds = milliseconds // 1_000
+    milliseconds -= seconds * 1_000
+
+    hours_marker = f"{hours:02d}:" if always_include_hours or hours > 0 else ""
+    return f"{hours_marker}{minutes:02d}:{seconds:02d}{decimal_marker}{milliseconds:03d}"
+
+def write_vtt(result, file):
+    print("WEBVTT\n", file=file)
+    for segment in result:
+        print(
+            f"{format_timestamp(segment['start'])} --> {format_timestamp(segment['end'])}\n"
+            f"{segment['text'].strip().replace('-->', '->')}\n",
+            file=file,
+            flush=True,
+        )
+
+def write_srt(result, file):
+    for i, segment in enumerate(result, start=1):
+        # write srt lines
+        print(
+            f"{i}\n"
+            f"{format_timestamp(segment['start'], always_include_hours=True, decimal_marker=',')} --> "
+            f"{format_timestamp(segment['end'], always_include_hours=True, decimal_marker=',')}\n"
+            f"{segment['text'].strip().replace('-->', '->')}\n",
+            file=file,
+            flush=True,
+        )
+
 def cli():
 
     import os
@@ -96,23 +134,6 @@ def cli():
         output_files = [[args.output]]
         if not os.path.isdir(os.path.dirname(args.output)):
             os.makedirs(os.path.dirname(args.output))
-
-    try:
-        # Old whisper version # Before https://github.com/openai/whisper/commit/da600abd2b296a5450770b872c3765d0a5a5c769
-        from whisper.utils import write_txt, write_srt, write_vtt
-
-    except ImportError:
-        # New whisper version
-        from whisper.utils import get_writer
-
-        def do_write(transcript, file, format):
-            writer = get_writer(format, os.path.curdir)
-            return writer.write_result({"segments": transcript}, file)
-        def get_do_write(format):
-            return lambda transcript, file: do_write(transcript, file, format)
-
-        write_srt = get_do_write("srt")
-        write_vtt = get_do_write("vtt")
 
     for fn, outputs in zip(input_files, output_files):
         with open(fn, "r", encoding="utf-8") as f:
